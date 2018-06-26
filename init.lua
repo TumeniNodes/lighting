8034ce5132b7891c053dd89375523067b3288037
TumeniNodes <tumeninodes@gmail.com>
2018-06-26 14:13:25 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,13 @@
+--[[
+
+    Lighting
+    ====================
+
+    Lighting mod for aufbauen
+
+    All credits, attributions, copyrights, licenses, are listed on individual .lua
+
+]]--
+
+
+dofile(minetest.get_modpath("lighting") .. "/streetlamp.lua")
